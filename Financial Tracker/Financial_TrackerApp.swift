e0253d0c9b5850edad62853def23a9183b3d6a9d
siamr <siam_1000@hotmail.com>
2022-11-18 11:34:47 -0500

    authentication complete, signup / login complete

--- Financial Tracker/Financial_TrackerApp.swift
+++ Financial Tracker/Financial_TrackerApp.swift
@@ -6,9 +6,16 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct Financial_TrackerApp: App {
+    
+    init(){
+        FirebaseApp.configure()
+    }
+    
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
