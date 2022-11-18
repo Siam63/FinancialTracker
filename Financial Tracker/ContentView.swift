6b7c98baa4bcaf251dd241caf21c8279a7468fd1
siamr <siam_1000@hotmail.com>
2022-11-18 10:10:06 -0500

    Initial Commit

---
+++ Financial Tracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Financial Tracker
+//
+//  Created by Siam Rahman on 2022-11-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Financial Tracker/Financial_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Financial_TrackerApp.swift
+//  Financial Tracker
+//
+//  Created by Siam Rahman on 2022-11-18.
+//
+
+import SwiftUI
+
+@main
+struct Financial_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
